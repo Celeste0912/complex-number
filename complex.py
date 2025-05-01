c9e422c932e06674d8c1ef776a8eff81c4a12a7c
SONG ZHENHUI <szh0912@hotmail.com>
2025-05-01 10:50:36 +0900

    all

--- complex.py
+++ complex.py
@@ -5,3 +5,7 @@ class Complex:
 
     def __add__(self, other):
         return Complex(self.a + other.a, self.b + other.b)
+    def __sub__(self, other):
+        return Complex(self.a - other.a, self.b - other.b)
+    def __mul__(self, other):
+        return Complex(self.a * other.a - self.b * other.b, self.a * other.b + self.b * other.a)
