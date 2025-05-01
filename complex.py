cc3775aa1d306f1e61acb1f563b6c0d98ed5e143
kyuhyuk <liguihe0924@naver.com>
2025-05-01 10:53:16 +0900

    __str__

--- complex.py
+++ complex.py
@@ -3,5 +3,8 @@ class Complex:
         self.a = a
         self.b = b 
 
+    def __str__(self):
+        return f'{self.a}, {self.b}i'
+
     def __add__(self, other):
         return Complex(self.a + other.a, self.b + other.b)
