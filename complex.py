4b486d2ad089122b009791f6f712ef4e21a8e977
kyuhyuk <liguihe0924@naver.com>
2025-05-01 11:10:39 +0900

    print

--- complex.py
+++ complex.py
@@ -1,14 +1,21 @@
 class Complex:
-    def __init__(self, a, b):
-        self.a = a
-        self.b = b 
+    def __init__(self, real, imaginary):
+        self.real = real
+        self.imaginary = imaginary
 
     def __str__(self):
-        return f'{self.a}, {self.b}i'
+        return f'{self.real}, {self.imaginary}i'
 
     def __add__(self, other):
-        return Complex(self.a + other.a, self.b + other.b)
+        return Complex(self.real + other.real, self.imaginary + other.imaginary)
     def __sub__(self, other):
-        return Complex(self.a - other.a, self.b - other.b)
+        return Complex(self.real - other.real, self.imaginary - other.imaginary)
     def __mul__(self, other):
-        return Complex(self.a * other.a - self.b * other.b, self.a * other.b + self.b * other.a)
+        return Complex(self.real * other.real - self.imaginary * other.imaginary, self.real * other.imaginary + self.imaginary * other.real)
+    
+A = Complex(1, 2)
+B = Complex(3, 4)
+
+print(A + B)
+print(A - B)
+print(A * B)
